68c871d18a7104724528a04e1c0a1ea1314538a5
Sheikh Fahad Shaon <fahadshaon09@gmail.com>
2023-02-28 00:41:02 +0600

    Dropdown added

--- test.js
+++ test.js
@@ -17,11 +17,15 @@ document.addEventListener('DOMContentLoaded', function () {
     };
 });
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('button').forEach(function (button) {
         button.onclick = function () {
             document.querySelector('#hello').style.color = button.dataset.color;
         };
     });
+
+    document.querySelector('select').onchange = function () {
+        document.querySelector('#hello').style.color = this.value;
+    }
 });
 
